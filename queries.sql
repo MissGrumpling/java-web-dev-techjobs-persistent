3a30e4ca28516f59e3a381db0faad9fc92399ee6
MissGrumpling <sjermel425@gmail.com>
2021-07-10 21:43:22 -0500

    queries schmeries

--- queries.sql
+++ queries.sql
@@ -4,7 +4,14 @@ id: int PK, employer: varchar(255), name: varchar(255), skills: varchar(255)
 ## Part 2: Test it with SQL
 SELECT name FROM employer WHERE location LIKE 'St. Louis City';
 
+SELECT name FROM techjobs.employer WHERE employer.location="St. Louis City";
+
 ## Part 3: Test it with SQL
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+
+SELECT DISTINCT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id=job_skills.skills_id
+ORDER BY name ASC;
